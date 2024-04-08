4fc27724d67671b5a097c16ea42e16cfd1946e90
chasomin <somin49@naver.com>
2024-04-08 23:25:53 +0900

    Feat: Fetch Single Result로 failure 받는 메서드 구현

--- BoxOfficeApp/Network/BoxOfficeNetwork.swift
+++ BoxOfficeApp/Network/BoxOfficeNetwork.swift
@@ -54,4 +54,43 @@ final class BoxOfficeNetwork {
             return Disposables.create()
         }.debug()
     }
+    
+    
+    static func fetchBoxOfficeSingleResult(date: String) -> Single<Result<Movie, APIError>> {
+        return Single<Result<Movie, APIError>>.create { single in
+            guard let url = URL(string: "https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=\(APIKey.key)&targetDt=\(date)") else {
+                single(.success(.failure(.invalidURL)))
+                print(APIError.invalidURL.rawValue)
+                return Disposables.create()
+            }
+            
+            URLSession.shared.dataTask(with: url) { data, response, error in
+                print("DataTask Succeed")
+                if let _ = error {
+                    print(APIError.unknownResponse.rawValue)
+                    single(.success(.failure(.unknownResponse)))
+                    return
+                }
+                
+                guard let response = response as? HTTPURLResponse,
+                      (200...299).contains(response.statusCode) else {
+                    print(APIError.statusError.rawValue)
+                    single(.success(.failure(.statusError)))
+                    return
+                }
+                
+                if let data = data,
+                   let appData = try? JSONDecoder().decode(Movie.self, from: data) {
+                    single(.success(.success(appData)))
+                } else {
+                    print(APIError.decodingError.rawValue)
+                    single(.success(.failure(.decodingError)))
+                }
+                
+            }.resume()
+            
+            return Disposables.create()
+        }
+    }
+
 }
